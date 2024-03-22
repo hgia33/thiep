a992628a1a57e89361d11838f5dc65c3dd0181c1
hgia33 <164437150+hgia33@users.noreply.github.com>
2024-03-22 07:36:37 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'KIM NGÂN';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng em bé';
+const contentCard = 'Chúc em bé sinh nhật vui vẻ. Chúc em ngày càng xinh đẹp,ngày càng dễ thương, học giỏi và yêu anh nhiềuu. Anh mong sẽ cùng em đón nhiều ngày sinh nhật khác nữa chứ không riêng ngày hôm nay. ANH YÊU EMM💘';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'uy.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
